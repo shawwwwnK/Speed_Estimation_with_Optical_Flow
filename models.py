ceb8374017e49a95a4a4c83f9ea89514fe1c7ffa
Shawn Kang <kangzx@stanford.edu>
2022-06-05 05:34:40 -0700

    generate_flow.py

--- models.py
+++ models.py
@@ -2,9 +2,9 @@ import torch
 import torch.nn as nn
 
 
-class OpticalFlowRegression(nn.Module):
+class OpticalFlowRegression_conv(nn.Module):
     def __init__(self, size):
-        super(OpticalFlowRegression, self).__init__()
+        super(OpticalFlowRegression_conv, self).__init__()
         self.H = size[0]
         self.W = size[1]
         self.conv_pitch = nn.Conv2d(2, 1, 1)
@@ -31,6 +31,50 @@ class OpticalFlowRegression(nn.Module):
         flatten_out_yaw = self.flatten(pool1_out_yaw)
         linear_out_yaw = self.linear_yaw(flatten_out_yaw)
         out_yaw = self.pool2(linear_out_yaw)
+        
+    
+        return torch.hstack((torch.abs(out_pitch), torch.abs(out_yaw)))
+
+class OpticalFlowRegression_tan(nn.Module):
+    def __init__(self, size):
+        super(OpticalFlowRegression_tan, self).__init__()
+        self.H = size[0]
+        self.W = size[1]
+
+        pool_size = 4
+        self.pool1 = nn.AvgPool2d(pool_size, stride=pool_size)
+        H_pool = (self.H - pool_size) // pool_size + 1
+        W_pool = (self.W - pool_size) // pool_size + 1
+        self.flatten = nn.Flatten()
+        
+        self.linear_x = nn.Linear(H_pool * W_pool, H_pool * W_pool)
+        self.linear_y = nn.Linear(H_pool * W_pool, H_pool * W_pool)
+        
+        self.linear_tan = nn.Linear(H_pool * W_pool, H_pool * W_pool)
+
+        self.linear_pitch = nn.Linear(H_pool * W_pool, H_pool * W_pool)
+        self.linear_yaw = nn.Linear(H_pool * W_pool, H_pool * W_pool)
+
+        self.pool2= nn.AvgPool1d(H_pool * W_pool)
+
+    def forward(self, x):               # x shape is Batch_size, 2, H, W
+        flow_x = x[:,0, :, :] 
+        flow_y = x[:,1, :, :] 
+        pool1_out_x = self.pool1(flow_x)
+        pool1_out_y = self.pool1(flow_y)
+        linear_out_x = self.linear_x(self.flatten(pool1_out_x))
+        linear_out_y = self.linear_y(self.flatten(pool1_out_y))
+        
+        tan = linear_out_x / linear_out_y
+        linear_out_tan = self.linear_tan(tan)
+        
+        arctan = torch.atan(linear_out_tan)
+
+        out_pitch = self.linear_pitch(arctan)
+        out_yaw = self.linear_yaw(arctan)
+        
+        out_pitch = self.pool2(out_pitch)
+        out_yaw = self.pool2(out_yaw)
         
     
         return torch.hstack((torch.abs(out_pitch), torch.abs(out_yaw)))
--- train_regression.py
+++ train_regression.py
@@ -4,7 +4,7 @@ import numpy as np
 from utils import *
 import time
 import os
-from regression import OpticalFlowRegression
+from models import *
 from dataset import FlowSpeedData
 
 
@@ -66,6 +66,90 @@ def train(train_dataloader, val_dataloader, epochs, lr, reg):
         val_losses.append(val_loss)
     
     return model, train_losses, val_losses
+
+def train_master(datapath, save_model_name, down_sample_rate=8, hyper_params=None, model_type="tan"):
+    if hyper_params is None:
+        epochs, lr, reg, eps, batch_size = 150, 1e-7, 1e-3 ,1e-6, 16
+    else:
+        epochs, lr, reg, eps, batch_size = hyper_params
+    train_set = FlowSpeedData(os.path.join(datapath, "all_flow"), os.path.join(datapath, "all"), 2, down_sample_rate)
+    train_set_size = int(len(train_set)*0.8)
+    val_set_size = len(train_set) - train_set_size
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    torch.manual_seed(10086)
+    train_set, val_set = torch.utils.data.random_split(train_set, [train_set_size, val_set_size])
+    train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    val_dataloader = torch.utils.data.DataLoader(val_set, batch_size=batch_size, shuffle=False)
+
+    a = next(iter(train_dataloader))
+    flow_size = a[0].shape[2:]
+
+    if model_type == "tan":
+        model = OpticalFlowRegression_tan(flow_size)
+    model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=reg, eps=eps)
+    loss_fn = torch.nn.MSELoss()
+
+    train_losses = []
+    val_losses = []
+
+    for epoch in range(epochs):
+        
+        print("Epoch", str(epoch + 1) + ": ", end="")
+        running_tloss = 0.0
+        
+        model.train(True)
+        for i, (flow, label) in enumerate(train_dataloader):
+            flow = flow.to(device)
+            label = label.to(device).to(torch.float32)
+            
+            
+            optimizer.zero_grad()
+            output = model(flow)
+            
+            
+            loss = loss_fn(output, label).to(torch.float32)
+            
+            loss.backward()
+            optimizer.step()
+            
+            running_tloss += loss.item()
+        train_loss = running_tloss / (i+1)
+            
+        model.train(False)
+
+        running_vloss = 0.0
+        for i, (vflow, vlabel) in enumerate(val_dataloader):
+            vflow = vflow.to(device)
+            vlabel = vlabel.to(device).to(torch.float32)
+            voutput = model(vflow)
+            vloss = loss_fn(voutput, vlabel)
+            running_vloss += vloss.item()
+
+        val_loss = running_vloss / (i+1)
+            
+            
+        print(f'Train Loss: {train_loss}. Validation Loss: {val_loss}')
+        train_losses.append(train_loss)
+        val_losses.append(val_loss)
+
+        results = {
+            "train_losses" : train_losses,
+            "val_losses" : val_losses,
+            "epochs" : epochs,
+            "lr" : lr,
+            "reg" : reg,
+            "eps" : eps, 
+            "down_sample_rate" : down_sample_rate,
+            "batch_size" : batch_size
+        }
+        results_path = os.path.join("results", save_model_name + ".txt")
+        model_path = os.path.join("results", save_model_name + ".pt")
+        torch.save(results, results_path)
+        torch.save(model.state_dict(), model_path)
+
+
 
 
 
